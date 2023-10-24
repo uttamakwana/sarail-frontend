f70e826e2874bcad1698c3b13dc076ad6c51822e
Uttam Makwana <uttamakwana4503@gmail.com>
2023-10-24 11:31:16 +0530

    Created folder structure of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+// import pages
+// import { Home } from "./pages";
 
 const App = () => {
   return (
     <main className="app">
-      <h1 className='heading mb-1 text-center underline'>This is SARAIL</h1>
+      <h1 className="heading mb-1 text-center underline">This is SARAIL</h1>
     </main>
   );
 };
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+import Home from "./Home/Home.jsx";
+export { Home };
