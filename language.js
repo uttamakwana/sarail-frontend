25d349ba9e42050024c62bcbca09b2995673513c
Gunjantank <121968343+Gunjantank@users.noreply.github.com>
2023-10-24 07:48:07 +0530

    Create language.js file which contains language name and logo

---
+++ language.js
@@ -0,0 +1,13 @@
+const languages = [
+  {
+    lang_name: "Gujarati",
+    lang_logo: "image_src_gujarati.png", // Replace "image_src_gujarati.png" with the actual image source URL or filename for Gujarati.
+  },
+  {
+    lang_name: "English",
+    lang_logo: "image_src_english.png", // Replace "image_src_english.png" with the actual image source URL or filename for English.
+  },
+  // Add more objects for other languages as needed.
+];
+
+module.exports = languages;
