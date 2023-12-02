c85716efa40c77ffa9369b85e2c7e473b30eb790
Uttam Makwana <uttamakwana4503@gmail.com>
2023-12-02 11:51:54 +0530

    Added necessary documents for future references. Ane wanted to make it open source.

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -6,7 +6,7 @@ const ContextProvider = ({ children }) => {
   // creating a loader with the loading state
   const [loadingState, setLoadingState] = useState(false);
   // selecting the language with the selectedLanguage state
-  const [selectedLanguage, setSelectedLanguage] = useState("English");
+  const [selectedLanguage, setSelectedLanguage] = useState("");
   // station name
   const [stationName, setStationName] = useState("");
 
